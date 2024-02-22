0f5d3aade76ff0e2238a7e89a12a2520c074e83d
Souza <bruno.higino.d.souza@accenture.com>
2024-02-22 20:04:18 -0300

    add the eks module

---
+++ main.tf
@@ -0,0 +1,41 @@
+resource "aws_eks_cluster" "main" {
+  count                     =  length(var.eks) 
+  name                      = var.eks[count.index].name
+  role_arn                  = var.eks[count.index].role_arn
+  enabled_cluster_log_types = var.eks[count.index].enabled_cluster_log_types
+  version                   = var.eks[count.index].version 
+  vpc_config {
+    subnet_ids              = var.eks[count.index].subnet_ids
+    endpoint_private_access = var.eks[count.index].endpoint_private_access
+    endpoint_public_access  = var.eks[count.index].endpoint_public_access
+    public_access_cidrs     = var.eks[count.index].public_access_cidrs
+    security_group_ids      = var.eks[count.index].security_group_ids
+  }
+
+  access_config {
+    authentication_mode                         = var.eks[count.index].authentication_mode
+    bootstrap_cluster_creator_admin_permissions = var.eks[count.index].bootstrap_cluster_creator_admin_permissions
+  }
+
+  encryption_config {
+    resources = var.eks[count.index].resource
+    provider {
+      key_arn = var.eks[count.index].key_arn
+    }
+  }
+
+  kubernetes_network_config {
+    service_ipv4_cidr = var.eks[count.index].service_ipv4_cidr
+    ip_family         = var.eks[count.index].ip_family
+  }
+
+  outpost_config {
+    control_plane_instance_type = var.eks[count.index].control_plane_instance_type
+    outpost_arns                = var.eks[count.index].outpost_arns
+    control_plane_placement {
+      group_name                = var.eks[count.index].group_name
+    }
+  }
+
+  tags = var.eks[count.index].tags
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "eks" {
+    type                                              = list(object({
+          name                                        = string
+          role_arn                                    = string
+          subnet_ids                                  = list(string)
+          endpoint_private_access                     = optional(string)
+          endpoint_public_access                      = optional(string)
+          public_access_cidrs                         = optional(list(string))
+          security_group_ids                          = optional(list(string))
+          authentication_mode                         = optional(string)
+          bootstrap_cluster_creator_admin_permissions = optional(string)
+          resources                                   = optional(list(string))
+          key_arn                                     = optional(string)
+          service_ipv4_cidr                           = optional(string)
+          ip_family                                   = optional(string)
+          control_plane_instance_type                 = optional(string)
+          outpost_arns                                = optional(string)
+          group_name                                  = optional(string)
+          tags                                        = map(string)
+        }
+      )
+    )
+  
+}
