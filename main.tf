3a2548fe72246ac0f59ff6df558f0c6ba0b1191b
bruno <brunohigino06@gmail.com>
2024-03-08 14:27:22 -0300

    add node group module

---
+++ main.tf
@@ -0,0 +1,44 @@
+resource "aws_eks_node_group" "eks_node_group" {
+  count                         = length(var.eks_node_group)  
+  cluster_name                  = var.eks_node_group[count.index].cluster_name
+  node_group_name               = var.eks_node_group[count.index].node_group_name
+  node_role_arn                 = var.eks_node_group[count.index].node_role_arn
+  subnet_ids                    = var.eks_node_group[count.index].subnet_ids
+  ami_type                      = var.eks_node_group[count.index].ami_type
+  capacity_type                 = var.eks_node_group[count.index].capacity_type
+  disk_size                     = var.eks_node_group[count.index].disk_size
+  force_update_version          = var.eks_node_group[count.index].force_update_version
+  instance_types                = var.eks_node_group[count.index].instance_types
+  labels                        = var.eks_node_group[count.index].labels
+  node_group_name_prefix        = var.eks_node_group[count.index].node_group_name_prefix
+  release_version               = var.eks_node_group[count.index].release_version
+  version                       = var.eks_node_group[count.index].version
+  taint {
+    key                         = var.eks_node_group[count.index].key
+    value                       = var.eks_node_group[count.index].value
+    effect                      = var.eks_node_group[count.index].effect
+  }
+  remote_access {
+    ec2_ssh_key                 = var.eks_node_group[count.index].ec2_ssh_key
+    source_security_group_ids   = var.eks_node_group[count.index].source_security_group_ids
+  }
+
+  launch_template {
+    id                          = var.eks_node_group[count.index].id
+    name                        = var.eks_node_group[count.index].name
+    version                     = var.eks_node_group[count.index].version
+  }
+
+  scaling_config {
+    desired_size                = var.eks_node_group[count.index].desired_size
+    max_size                    = var.eks_node_group[count.index].max_size
+    min_size                    = var.eks_node_group[count.index].min_size
+  }
+
+  update_config {
+    max_unavailable             = var.eks_node_group[count.index].max_unavailable
+    max_unavailable_percentage  = var.eks_node_group[count.index].max_unavailable_percentage
+  }
+
+  tags                          = var.eks_node_group[count.index].tags
+}
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "eks_node_group" {
+  type = list(object({
+    cluster_name                = string
+    node_group_name             = string
+    node_role_arn               = string
+    subnet_ids                  = list(string)
+    ami_type                    = optional(string) 
+    capacity_type               = optional(string)
+    disk_size                   = optional(string) 
+    force_update_version        = optional(string) 
+    instance_types              = optional(string)
+    labels                      = optional(list(string))
+    node_group_name_prefix      = optional(string)
+    release_version             = optional(string)
+    version                     = optional(string)
+    key                         = optional(string) 
+    value                       = optional(string)
+    effect                      = optional(string)
+    ec2_ssh_key                 = optional(string) 
+    source_security_group_ids   = optional(list(string)) 
+    id                          = optional(string)
+    name                        = optional(string)
+    version                     = optional(string)
+    desired_size                = string
+    max_size                    = string
+    min_size                    = string
+    max_unavailable             = optional(string)
+    max_unavailable_percentage  = optional(string)
+    tags                        = map(string)
+  }))
+}
