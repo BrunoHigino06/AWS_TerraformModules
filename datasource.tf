b3a2e1e4ede9c9b7a686806c009c99a1c0b69e18
bruno <brunohigino06@gmail.com>
2024-06-21 18:10:56 -0300

    fix the db_subnet_group

--- datasource.tf
+++ datasource.tf
@@ -2,7 +2,7 @@ locals {
   subnet_unique_names     = flatten([for subnet_group in var.db_subnet_group : subnet_group.subnet_unique_name])
 }
 
-data "aws_subnet" "data_subnet_id" {
+data "aws_subnet" "subnet" {
   for_each = { for subnet in local.subnet_unique_names : subnet => subnet ... if subnet != null}
 
   filter {
