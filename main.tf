0bc0472775612fb6065bd483ac6c09f57daa6af8
bruno <brunohigino06@gmail.com>
2024-06-21 18:38:46 -0300

    fix the db instance module

--- main.tf
+++ main.tf
@@ -6,16 +6,16 @@ resource "random_password" "db_password" {
 
 resource "aws_db_instance" "db_instance" {
     count                   = length(var.db_instance)      
-    allocated_storage       = var.db_instance[count.indes].allocated_storage
-    db_name                 = var.db_instance[count.indes].db_name
-    engine                  = var.db_instance[count.indes].engine
-    engine_version          = var.db_instance[count.indes].engine_version
-    instance_class          = var.db_instance[count.indes].instance_class
-    username                = var.db_instance[count.indes].username
+    allocated_storage       = var.db_instance[count.index].allocated_storage
+    db_name                 = var.db_instance[count.index].db_name
+    engine                  = var.db_instance[count.index].engine
+    engine_version          = var.db_instance[count.index].engine_version
+    instance_class          = var.db_instance[count.index].instance_class
+    username                = var.db_instance[count.index].username
     password                = random_password.db_password.result
-    parameter_group_name    = var.db_instance[count.indes].parameter_group_name
-    skip_final_snapshot     = var.db_instance[count.indes].skip_final_snapshot
-    db_subnet_group_name    = var.db_instance[count.indes].db_subnet_group_name
+    parameter_group_name    = var.db_instance[count.index].parameter_group_name
+    skip_final_snapshot     = var.db_instance[count.index].skip_final_snapshot
+    db_subnet_group_name    = var.db_instance[count.index].db_subnet_group_name
     tags                    = merge(
                               var.db_instance[count.index].tags,
         {name               = var.db_instance[count.index].name}
